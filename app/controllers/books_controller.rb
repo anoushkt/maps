a09336ba51e6562f0dff67468207d4a1bec53f28
Anoushka Tiwari <anoushkt@andrew.cmu.edu>
2019-08-05 15:46:20 -0400

    classes decorated

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,11 +9,6 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new
-    @book.af= params[:book][:af]
-    @book.protocol=params[:book][:protocol]
-    @book.country=params[:book][:country]
-    @book.save
 
   end
 end
