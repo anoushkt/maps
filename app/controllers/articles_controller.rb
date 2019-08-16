c604b7d5577d722c96015143b83373a48ced3cd8
Anoushka Tiwari <anoushkt@andrew.cmu.edu>
2019-08-16 15:50:04 -0400

    added colors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,27 +1,20 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+  
   end
 
   def show
-    @articles= Article.find(params[:id])
-
 
   end
 
   def new
-    @article = Article.new
+
 
 
   end
 
 
   def create
-    @article = Article.new(
-    title: params[:article][:title],
-    body: params[:article][:body])
-    @article.save
-    redirect_to article_path(@article)
 
   end
 
